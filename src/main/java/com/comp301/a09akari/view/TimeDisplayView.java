9e14063ab661a80351ebe79fb78a35981983bd68
Javier lorenzo <92158938+alu0101321257@users.noreply.github.com>
2023-12-05 11:17:36 +0000

    Update TimeDisplayView.java (doc)

--- src/main/java/com/comp301/a09akari/view/TimeDisplayView.java
+++ src/main/java/com/comp301/a09akari/view/TimeDisplayView.java
@@ -1,7 +1,25 @@
+/**
+ * @file TimeDisplayView.java
+ * @brief Interface for rendering a view that displays the final time in the Akari Puzzle Game.
+ *
+ * This interface defines the contract for rendering a view that displays the final time in the Akari Puzzle Game.
+ *
+ * @package com.comp301.a09akari.view
+ */
 package com.comp301.a09akari.view;
 
 import javafx.scene.Parent;
-
+/**
+ * @brief Interface for rendering a view that displays the final time in the Akari Puzzle Game.
+ *
+ * This interface defines the contract for rendering a view that displays the final time in the Akari Puzzle Game.
+ */
 public interface TimeDisplayView {
+    /**
+     * @brief Renders the time display view.
+     *
+     * @param finalTime The final time to be displayed.
+     * @return The root node for the time display view.
+     */
     Parent render(double finalTime);
 }
