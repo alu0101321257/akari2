975b86819820f251113543e10f13ef3a46f88186
Javier lorenzo <92158938+alu0101321257@users.noreply.github.com>
2023-12-05 10:54:32 +0000

    Update ModelObserver.java (doc)

--- src/main/java/com/comp301/a09akari/model/ModelObserver.java
+++ src/main/java/com/comp301/a09akari/model/ModelObserver.java
@@ -1,6 +1,26 @@
+/**
+ * @file ModelObserver.java
+ * @brief Interface for observing changes in a puzzle model.
+ *
+ * This interface defines a method that should be implemented by classes interested in observing changes in a puzzle model.
+ * When a model value is changed, the `update()` method is called on all active ModelObserver objects.
+ *
+ * @package com.comp301.a09akari.model
+ */
 package com.comp301.a09akari.model;
-
+/**
+ * @brief Interface for observing changes in a puzzle model.
+ *
+ * This interface defines a method that should be implemented by classes interested in observing changes in a puzzle model.
+ * When a model value is changed, the `update()` method is called on all active ModelObserver objects.
+ */
 public interface ModelObserver {
-  /** When a model value is changed, the model calls update() on all active ModelObserver objects */
+  /**
+   * @brief Notifies the observer that a change has occurred in the puzzle model.
+   *
+   * This method is called by the puzzle model when a change occurs, allowing the observer to react accordingly.
+   *
+   * @param model The puzzle model that has been updated.
+   */
   void update(Model model);
 }
