e2bdd7021ebc5438b7a4f87aecde6a4891eba6f2
Javier lorenzo <92158938+alu0101321257@users.noreply.github.com>
2023-12-05 11:00:45 +0000

    Update PuzzleImpl.java (doc)

--- src/main/java/com/comp301/a09akari/model/PuzzleImpl.java
+++ src/main/java/com/comp301/a09akari/model/PuzzleImpl.java
@@ -1,20 +1,53 @@
+/**
+ * @file PuzzleImpl.java
+ * @brief Implementation of the Puzzle interface for representing a puzzle board.
+ *
+ * This class implements the Puzzle interface, providing methods to access information about the puzzle board,
+ * including dimensions, cell types, and clues.
+ *
+ * @package com.comp301.a09akari.model
+ */
 package com.comp301.a09akari.model;
-
+/**
+ * @brief Implementation of the Puzzle interface for representing a puzzle board.
+ *
+ * This class implements the Puzzle interface, providing methods to access information about the puzzle board,
+ * including dimensions, cell types, and clues.
+ */
 public class PuzzleImpl implements Puzzle {
   int[][] board;
-
+  /**
+   * @brief Constructor for the PuzzleImpl class.
+   *
+   * @param board The 2D array representing the puzzle board.
+   */
   public PuzzleImpl(int[][] board) {
     this.board = board;
   }
-
+  /**
+   * @brief Gets the width of the puzzle board.
+   *
+   * @return The width of the puzzle board.
+   */
   public int getWidth() {
     return this.board[0].length;
   }
-
+  /**
+   * @brief Gets the height of the puzzle board.
+   *
+   * @return The height of the puzzle board.
+   */
   public int getHeight() {
     return this.board.length;
   }
-
+  /**
+   * @brief Gets the cell type at the specified position on the puzzle board.
+   *
+   * @param r The row of the cell.
+   * @param c The column of the cell.
+   * @return The cell type at the specified position.
+   * @throws IndexOutOfBoundsException if r or c is out of bounds.
+   */
   public CellType getCellType(int r, int c) {
     if (this.board[r][c] > 6 || this.board[r][c] < 0) {
       throw new IndexOutOfBoundsException();
@@ -30,7 +63,15 @@ public class PuzzleImpl implements Puzzle {
     }
     return null;
   }
-
+  /**
+   * @brief Gets the clue value at the specified position on the puzzle board.
+   *
+   * @param r The row of the clue.
+   * @param c The column of the clue.
+   * @return The clue value at the specified position.
+   * @throws IllegalArgumentException if the cell is a wall or corridor.
+   * @throws IndexOutOfBoundsException if r or c is out of bounds.
+   */
   public int getClue(int r, int c) {
     if (this.board[r][c] == 5 || this.board[r][c] == 6) {
       throw new IllegalArgumentException();
