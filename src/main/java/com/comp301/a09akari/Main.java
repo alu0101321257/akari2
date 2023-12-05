b5675af948c423668a669ef3a0e70c9ca4e701b6
Javier lorenzo <92158938+alu0101321257@users.noreply.github.com>
2023-12-05 11:30:56 +0000

    Update Main.java (doc)

--- src/main/java/com/comp301/a09akari/Main.java
+++ src/main/java/com/comp301/a09akari/Main.java
@@ -1,11 +1,20 @@
+/**
+ * @file Main.java
+ * The entry point for the Akari puzzle game application.
+ */
 package com.comp301.a09akari;
 
 import com.comp301.a09akari.view.AppLauncher;
 import javafx.application.Application;
 
+
 public class Main {
+  /**
+   * The main method that launches the JavaFX application using AppLauncher.
+   *
+   * @param args Command-line arguments (not used in this application).
+   */
   public static void main(String[] args) {
-       //Nueva ramito
-    Application.launch(AppLauncher.class);
+    Application.launch(AppLauncher.class); // Launch the JavaFX application using AppLauncher
   }
 }
